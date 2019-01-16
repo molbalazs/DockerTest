49535d42005c388b630ee7c973d0a64e8f6f6d7e
Molnár Balázs <balazs.molnar@accedo.tv>
2019-01-16 20:29:29 +0000

    Add mount folder and copy the built folder structure into it

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Ubuntu 18.04, Apache Maven 3.3.9, OpenJDK 8, git - proof of concept
 
 docker build -t="maven" https://github.com/molbalazs/DockerTest.git
 
-docker run -ti maven
+# Run for all platforms
+docker run -tiv ~/SHARED:/VMSHARED maven
+
+# Run for one platform
+docker run -tiv ~/SHARED:/VMSHARED maven amazonfire
--- app.sh
+++ app.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-git clone git@gitlab.channel4.com:bigscreen/ConnectedTVApplication.git && \
-cd ConnectedTVApplication && \
+git clone "git@gitlab.channel4.com:bigscreen/ConnectedTVApplication.git" && cd "ConnectedTVApplication";
+if [ $1 = "all" ]
+then
+#Run maven script for all platforms
+mvn package
+else
+#Run maven script for one platform
 mvn package -pl $1
+fi
+
+#Copy the whole repository into the mounted folder
+cp -r ../ConnectedTVApplication /VMSHARED
